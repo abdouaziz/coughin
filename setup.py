222b43a294162dbb541c63c95ab4acb76bdd0c08
abdouaziz <abdouaziz@gmail.com>
2021-04-16 13:26:24 +0000

    Coughin application

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This a projet , from a cough to know if person had covid or no ',
+    author='Abdou Aziz DIOP',
+    license='MIT',
+)
